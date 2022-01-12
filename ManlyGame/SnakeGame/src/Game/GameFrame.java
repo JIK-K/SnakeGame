97dd94849c59cf82f233e30ab6b4140784a3c8a2
JIK-K <87060676+JIK-K@users.noreply.github.com>
2022-01-12 16:21:57 +0900

    메인화면 추가
    +MainPanel.class
    +ViewController.class

--- ManlyGame/SnakeGame/src/Game/GameFrame.java
+++ ManlyGame/SnakeGame/src/Game/GameFrame.java
@@ -3,13 +3,16 @@ package Game;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
+	ViewController controller;
 	public GameFrame() {
-		this.add(new GamePanel());
+		this.add(new MainPanel());
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
+        
+        controller = new ViewController(this);
     }
 }
