88270381ac29f54f16b72aff6f5d7021fc7567b1
JIK-K <87060676+JIK-K@users.noreply.github.com>
2022-02-07 19:42:15 +0900

    오류수정

--- ManlyGame/SnakeGame/src/Game/GamePanel.java
+++ ManlyGame/SnakeGame/src/Game/GamePanel.java
@@ -54,10 +54,11 @@ public class GamePanel extends JPanel implements ActionListener{
     
     public void draw(Graphics g){
         if(running){
-            for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
-                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-                g.drawLine(0, i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
-            }
+//        	DebugLine
+//            for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+//                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+//                g.drawLine(0, i*UNIT_SIZE, SCREEN_HEIGHT, i*UNIT_SIZE);
+//            }
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
